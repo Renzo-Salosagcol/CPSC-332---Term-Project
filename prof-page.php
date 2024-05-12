936a9a3f3460b726a116b10ef7e7806732889cbc
Matt De Binion <debinionm@gmail.com>
2024-05-11 18:25:54 -0700

    Added grades querying (needs to be checked with actual data)

--- prof-page.php
+++ prof-page.php
@@ -11,38 +11,53 @@
   <header>
     <a href="index.php"><button>Back</button></a>
   </header>
-  <?php
-  require_once './query/professor.php';
-  ?>
   <div>
     <div class="section-1">
       <p>Want a list of titles, classrooms, meeting days, and time of a professor's classes?</p>
-      <p>Please input their social security number: <input type="number"></p>
-      <button>Show List</button>
+      <p>Please input their social security number: <input type="number" id="professor-ssn"></p>
+      <button id="ssn-button">Show List</button>
       <script>
         $(document).ready(function () {
-          $("button").click(function () {
-            var ssn = $("input[type='number']").val();
+          $("#ssn-button").click(function () {
+            var ssn = $('#professor-ssn').val();
             $.ajax({
               url: './query/professor.php',
               type: 'POST',
               data: { ssn: ssn },
               success: function (response) {
                 console.log("Returned: " + response);
-                $(".sql-query").html(response);
+                $(".classes-query").html(response);
               }
             });
           });
         });
       </script>
-      <p class="sql-query"></p> 
+      <p class="classes-query"></p> 
     </div>
     <div class="section-2">
       <p>Want a count of students and get each distinct grade?</p>
       <p>Please input the following:</p>
-      <p>Course Number: <input type="number"></p>
-      <p>Section Number: <input type="number"></p>
-      <button>Show Results</button>
+      <p>Course Number: <input type="number" id="course-num"></p>
+      <p>Section Number: <input type="number" id="section-num"></p>
+      <button id="grades-button">Show Results</button>
+      <script>
+        $(document).ready(function () {
+          $("#grades-button").click(function () {
+            var courseNum = $('#course-num').val();
+            var sectionNum = $('#section-num').val();
+            $.ajax({
+              url: './query/grades.php',
+              type: 'POST',
+              data: { CourseNumber: courseNum, SectionNumber: sectionNum },
+              success: function (response) {
+                console.log("Returned: " + response);
+                $(".grades-query").html(response);
+              }
+            });
+          });
+        });
+      </script>
+      <p class="grades-query"></p> 
     </div>
   </div>
 </body>
